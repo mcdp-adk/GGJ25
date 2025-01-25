bd422d2d9f93f4d370eea9aa31155a0b5e9bf7f5
MCDP <mcdp2alh@gmail.com>
2025-01-25 11:33:12 +0800

    Add spawn point

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject prefabToSpawn;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        SpawnAtPoint();
+    }
+
+    public void SpawnAtPoint()
+    {
+        if (spawnPoint != null && prefabToSpawn != null)
+        {
+            Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
 }
